e9a8cb4ce41c00e8593b68bda661c276136c2a67
yogeshcoded <yogeshbyjava@gmail.com>
2024-07-23 10:37:57 +0530

    update

--- src/main/java/com/nt/basic/Test010QuickSort.java
+++ src/main/java/com/nt/basic/Test010QuickSort.java
@@ -1,4 +1,23 @@
 package com.nt.basic;
 
+import java.util.Stack;
+
 public class Test010QuickSort {
+    public static void main(String[] args) {
+        String first = "listen";
+        String second = "liste";
+        char[] characters = first.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder(second);
+
+        for(Character ch : characters) {
+            int index = stringBuilder.indexOf(""+ch);
+            if(index != -1) {
+                stringBuilder.deleteCharAt(index);
+            }else {
+                break;
+            }
+        }
+        System.out.println(stringBuilder.isEmpty() ? "Anagram" : "Not an anagram");
+
+    }
 }
---
+++ src/main/java/com/nt/basic/Test06BubbleSortAlgorithm.java
@@ -0,0 +1,41 @@
+package com.nt.basic;
+
+import java.util.Arrays;
+
+public class Test06BubbleSortAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {35, 5, 7, 45, 60, 4};
+        int[] a = {5, 7, 45, 60};
+        //call sorting method
+        bubbleSort(a);
+        //call print method
+        printArray(a);
+
+    }//main()
+
+    public static void bubbleSort(int[] arr) {
+        boolean isSwap;
+        //iterate the array from o to n
+        for (int i = 0; i < arr.length - 1; i++) {
+            isSwap = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                //comparing each pair of elements
+                if (arr[j] > arr[j + 1]) {       // e.g. 35>5 => true
+                    //if conditions is true then swap the numbers
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isSwap = true;
+                }
+            }
+            if (!isSwap)
+                break;
+        }
+
+    }//bubbleSort()
+
+    public static void printArray(int[] array) {
+        Arrays.stream(array).forEach(p -> System.out.print(p + " "));
+    }
+
+}
